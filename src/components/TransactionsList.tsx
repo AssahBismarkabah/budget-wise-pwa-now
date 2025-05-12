3bf89744c180356a86d5a3930bb3506dd5d7c197
AssahBismarkabah <assah9799@gmail.com>
2025-05-12 18:09:10 +0100

    OpenBankingGateway integration

--- src/components/TransactionsList.tsx
+++ src/components/TransactionsList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { aisService, Transaction } from '../services/aisService';
 
 interface TransactionsListProps {
